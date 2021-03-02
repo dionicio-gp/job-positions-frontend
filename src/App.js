a3195567f069dc5511e42c47907ebabbc8acdbca
Dionicio Diaz <dionicio007@hotmail.com>
2021-03-01 23:41:33 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect, useCallback } from "react";
+import { CITIES, DESCRIPTIONS}  from './constants/commons';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [jobPosts, setJobPosts] = useState([]);
+  const [description, setDescription] = useState('');
+  const [city, setCity] = useState('');
+
+  const getJobPost = useCallback(
+    () => {
+      fetch(`http://127.0.0.1:3333/positions?description=${description}&location=${city}`)
+        .then(response => response.json())
+        .then(data => setJobPosts(data));
+    },
+    [description,city],
+  )
+
+useEffect(() => {
+  if(description || city){
+      getJobPost();
+    }
+}, [description, city, getJobPost])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <label>
+        Job Description:
+        <input list="description" value={description} onChange={(event) => setDescription(event.target.value)} />
+        <datalist id="description">
+          {DESCRIPTIONS.map((description) => <option value={description} />)}
+        </datalist>
+      </label>
+      <label>
+        Location:
+        <input  list="location" value={city} onChange={(event) => setCity(event.target.value)} />
+        <datalist id="location">
+          {CITIES.map((jobCity) =>  <option value={jobCity} />)}
+        </datalist>
+      </label>
       </header>
+      <div className="jobs-container">
+          {
+            jobPosts.map((jobPost) => {
+
+
+            return ( <details>
+                  <summary> {jobPost.title}</summary>
+                  <p><a href={jobPost.company_url}> {jobPost.company} </a></p>
+                  <p  dangerouslySetInnerHTML={{
+                      __html: jobPost.description
+                    }}></p>
+                </details>
+                )
+          })
+          }
+      </div>
     </div>
   );
 }
