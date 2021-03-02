73a63f66de542415e20d25b4874bf86d1065e468
Dionicio Diaz <dionicio007@hotmail.com>
2021-03-02 14:23:15 -0400

    rename cities constant

--- src/constants/commons.js
+++ src/constants/commons.js
@@ -1,2 +1,2 @@
-export const CITIES =  ['Chicago', 'San Francisco', 'Phoenix', 'London', 'Beijing', 'Paris'];
+export const LOCATIONS =  ['Chicago', 'San Francisco', 'Phoenix', 'London', 'Beijing', 'Paris'];
 export const DESCRIPTIONS = ['Javascript', 'Java', 'Python', 'React', 'Ruby', 'Go'];
